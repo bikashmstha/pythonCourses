5b1d53f588f72864823e81d4b445d9fb5645c27e
Ubuntu <ubuntu@ip-172-31-13-190.us-west-2.compute.internal>
2016-12-16 04:49:12 +0000

    updating from terminal via server

--- CoursesAssignment/settings.py
+++ CoursesAssignment/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+ALLOWED_HOSTS = ['54.214.223.155']
